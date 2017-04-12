9bf1660470e4d4733fa1959c5f30c8659e0327de
quantumduck <quantum.duck@hotmail.com>
2017-04-12 13:33:54 -0400

    Question 1

--- sql_assignment.sql
+++ sql_assignment.sql
@@ -5,7 +5,7 @@
 -- below each of the following questions:
 
 -- 1) Find the albums recorded by the artist Queen.
-
+SELECT title FROM albums WHERE artist_id = (SELECT id FROM artists WHERE name = 'Queen');
 
 
 -- 2) [Count](http://www.w3schools.com/sql/sql_func_count.asp) how many tracks belong to the media type "Protected MPEG-4 video file".
